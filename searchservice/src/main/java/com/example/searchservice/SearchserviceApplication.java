deef4b87ddc5c3059faa18d1f3a71888eb6d6a70
Mallika-Dey <mallikaju307@gmail.com>
2024-01-30 14:01:28 +0600

    added webresource config to search

--- searchservice/src/main/java/com/example/searchservice/SearchserviceApplication.java
+++ searchservice/src/main/java/com/example/searchservice/SearchserviceApplication.java
@@ -2,8 +2,10 @@ package com.example.searchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class SearchserviceApplication {
 
 	public static void main(String[] args) {
